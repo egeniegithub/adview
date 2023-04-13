3c85138e89216bd87d56578623fa0cc7173c9ddd
muhammad muzammil <muzamil.egenie@gmail.com>
2023-04-13 20:19:44 +0500

    buuble script added

---
+++ frontend/src/Services/BubbleIo.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+
+export const getBubbleUsers = () => {
+
+    const site_uri = 'https://testgoogleads.bubbleapps.io/version-test/api/1.1/obj'
+    const token = '68c574be9db0fe5aea000eb325eb28ae'
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: site_uri,
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    };
+
+    axios.request(config)
+        .then((response) => {
+            return response.data
+            // console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+            console.log(error.response.data);
+        });
+
+}
